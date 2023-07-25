b4e857561d7f165d1eb84ecc7164d85d75c0158a
sumith <poonersumith9@gmail.com>
2023-07-25 09:05:23 +0530

    v1

---
+++ home/footer.php
@@ -0,0 +1,26 @@
+<!-- copyright and contactUs fixed footer -->
+
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+.footer {
+    position: fixed;
+    width: 100%;
+    text-align: center;
+    
+    bottom: 0;
+    background-color: #2ab7ca;
+    color: white;
+}
+</style>
+</head>
+<body>
+<footer class="footer">
+
+  <p style="margin-top: 4px;margin-bottom:-1px">Designed By Sumith</p><br>
+
+</footer>
+
+</body>
+</html>
